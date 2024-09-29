a09220855b7bb163c696c17aa7fa9d508b4a677f
quan.bui <quan.bui@dreambroker.fi>
2024-09-29 10:34:52 +0300

    logic for track recommendation with given data

--- controller/trackController.js
+++ controller/trackController.js
@@ -3,19 +3,54 @@ import { heartRateData, tracks } from "../data"
 const purposes = ["Relax" , "Focus", "Creativity", "Deep Relaxation", "Work"]
 
 const findTrackByPurpose = (purpose) => {
-
+    let purposeTracks = [];
+    if (purposes.includes(purpose)) {
+        tracks.map(track => {
+          if (track.purpose === purpose) {
+            purposeTracks.push(track)
+          }
+        })
+    }
+    return purposeTracks
 }
 
 const getTrackRecommendation = (heartRate) => {
-
-}
-
-const getPurposes = (req, res) => {
-    
+    if (heartRate < 60) {
+        recommendations = tracks.filter(track => 
+            track.binaural_beat === "Theta" //frequency
+        )
+    } else if (heartRate >= 60 && heartRate < 80) {
+        recommendations = tracks.filter(track => 
+            track.binaural_beat === "Alpha"
+        );
+    } else if (heartRate >= 80 && heartRate < 100) {
+        recommendations = tracks.filter(track => 
+            track.binaural_beat === "Beta"
+        );
+    } else {
+        recommendations = tracks.filter(track => 
+            track.binaural_beat === "Gamma"
+        );
+    }
+  return recommendations
 }
 
 const recommend = (req, res) => {
-
+    const {purpose} = req.query
+    if (!purpose) {
+        const recommendations = heartRateData.map(data => {
+            const recommendation = getTrackRecommendation(data.value);
+            return {
+              timestamp: data.sdate,
+              heartRate: data.value,
+              recommendation: recommendation
+            };
+          });
+        return res.json({recommendations})
+    } else {
+        const recommendations = findTrackByPurpose(purpose)
+        res.json({recommendations})
+    }
 }
 
 
---
+++ controller/trackController2.js
@@ -0,0 +1,31 @@
+const purposes = {
+    morning: ["Work", "Creativity", "Focus"],
+    afternoon: ["Focus", "Creativity", "Work"],
+    night: ["Relax", "Deep Relaxation", "Meditation", "Sleep"]
+};
+
+function getPurposesBasedOnTime() {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 6 && currentHour < 12) {
+      // Morning: 6 AM to 12 PM
+      return purposes.morning;
+    } else if (currentHour >= 12 && currentHour < 18) {
+      // Afternoon: 12 PM to 6 PM
+      return purposes.afternoon;
+    } else {
+      // Night: 6 PM to 6 AM
+      return purposes.night;
+    }
+}
+
+
+const getPurposes = (req, res) => {
+  const currentTime = new Date();
+  const suitablePurposes = getPurposesBasedOnTime();
+  res.json({
+    greeting: `Good ${currentTime.getHours() < 12 ? 'morning' : (currentTime.getHours() < 18 ? 'afternoon' : 'evening')}`,
+    message: "What are you looking for?",
+    purposes: suitablePurposes
+  });
+}
+
